d84ce887bdf75e32311cff1a0a9f0ec31a5d13b8
xaphaniafox <f.hosseini64@gmail.com>
2023-07-12 11:42:26 +0300

    Lesson: Props (passing data throught nested components)

--- src/App.jsx
+++ src/App.jsx
@@ -3,61 +3,69 @@ const welcome = {
   title: "React",
 };
 
-const list = [
-  {
-    id: 0,
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Reza Ahmadi",
-    num_comments: 3,
-    points: 4,
-  },
-  {
-    id: 1,
-    title: "Readux",
-    url: "https://redux.js.org",
-    author: "Mohammad Kia",
-    num_comments: 2,
-    points: 5,
-  },
-];
-
 function App() {
+  const stories = [
+    {
+      id: 0,
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Reza Ahmadi",
+      num_comments: 3,
+      points: 4,
+    },
+    {
+      id: 1,
+      title: "Readux",
+      url: "https://redux.js.org",
+      author: "Mohammad Kia",
+      num_comments: 2,
+      points: 5,
+    },
+  ];
   return (
     <>
       <h1>
         {welcome.greeting} {welcome.title}
       </h1>
       <Search />
-      <List />
+      <List list={stories} />
     </>
   );
 }
+
+const handleChange = (event) => {
+  console.log(event.target.value);
+};
 
 const Search = () => {
   return (
     <div>
       <label htmlFor="search">Search</label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange} />
     </div>
   );
 };
 
-const List = () => {
+const List = (props) => {
   return (
     <ul>
-      {list.map((user) => (
-        <li key={user.id}>
-          <span>
-            <a href={user.url}>{user.title}</a>
-          </span>
-          <span>{user.author}</span>
-          <span>{user.num_comments}</span>
-          <span>{user.points}</span>
-        </li>
+      {props.list.map((user) => (
+        <Item key={user.id} item={user} />
       ))}
     </ul>
   );
 };
 
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 export default App;
