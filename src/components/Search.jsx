03aec00c949334471c41fd8b42a3d00d562ff28e
xaphaniafox <f.hosseini64@gmail.com>
2023-07-12 12:59:35 +0300

    Lesson: States

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const handleChange = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
   return (
     <div>
       <label htmlFor="search">Search</label>
       <input type="text" id="search" onChange={handleChange} />
+      <p>{searchTerm}</p>
     </div>
   );
 };
